e02d1ffe072db00acf0fa1bf2a9ebea3dcb65ea7
Ahmed Serifoski <ahmed.serifoski@gmail.com>
2019-10-29 09:43:07 +0100

    altered script.js

--- script.js
+++ script.js
@@ -19,23 +19,5 @@ function textSize() {
     }
 }
 
-//cards values
-
-//Change the font size of the sample fields
-function textSize() {
-    let text = 'I cannot think of souch a deep thaught for an example text';
-    let selectArea = document.querySelector(".sizes");
-    let sampleText = document.querySelectorAll(".display");
-    let ahmed = "ahmed";
-
-    for (i = 0; i < selectArea.length*2; i ++) {
-        sampleText = text;
-
-        
-
-    }
-    console.log(text);
-}
-console.log(text);
 
 
